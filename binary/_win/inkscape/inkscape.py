edaa26dffad432397de8adee327e61da0a98a37e
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2022-11-15 11:56:57 +0100

    only provide x86_64 version of inkscape
    makes it compatible with recent changes in craft upstream project
    
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- binary/_win/inkscape/inkscape.py
+++ binary/_win/inkscape/inkscape.py
@@ -6,10 +6,7 @@ from Package.BinaryPackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        if CraftCore.compiler.isX64():
-            self.targets["1.1"] = f"https://inkscape.org/gallery/item/26934/inkscape-1.1-x{CraftCore.compiler.bits}.7z"
-        else:
-            self.targets["1.1"] = f"https://inkscape.org/gallery/item/26936/inkscape-1.1-x{CraftCore.compiler.bits}.7z"
+        self.targets["1.1"] = f"https://inkscape.org/gallery/item/26934/inkscape-1.1-x64.7z"
         self.targetInstSrc["1.1"] = f"inkscape"
         self.webpage = "https://inkscape.org/"
         self.description = "A powerful, free design tool"
